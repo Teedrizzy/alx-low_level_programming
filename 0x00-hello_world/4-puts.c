718ebd98493efa4ad04f033d2b9cc1b1ad0b3ca9
Teedrizzy <Teedrizzy20@gmail.com>
2023-08-10 21:30:55 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -the fuction is to print out output
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
