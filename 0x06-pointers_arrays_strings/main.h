e1b611b91dc8c99e5b75bb5f749b9194e5a2c7de
Teedrizzy <Teedrizzy20@gmail.com>
2023-08-24 20:45:54 -0700

    magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ char *cap_string(char *a);
 char *leet(char *a);
 int _putchar(char c);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
