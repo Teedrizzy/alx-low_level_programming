a479e1b7073ae1b20b75d643b60d5e3d4b029a5e
Teedrizzy <Teedrizzy20@gmail.com>
2023-08-24 21:30:19 -0700

    function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers.
- * @a: input array.
- * @n: input n elements
- * Return: no return.
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
 
-void print_array(int *a, int n)
+int _atoi(char *s)
 {
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	for (; i < n; i++)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		printf("%d", *(a + i));
-		if (i != (n - 1))
-			printf(", ");
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	printf("\n");
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
