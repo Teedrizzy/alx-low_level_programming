3b53f447abeb7685ed2252a73033867d6cfc0339
Teedrizzy <teedrizzy20@gmail.com>
2023-09-21 21:01:45 -0700

    function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,7 +6,6 @@
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
- *
  * Return: the address of the new element, or NULL if it fails
  */
 
